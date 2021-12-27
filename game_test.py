2efbdffbc16ee0c51748057544a5d2bfcfec615a
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-12-27 10:35:24 +0000

    pep8 formatted tests

--- game_test.py
+++ game_test.py
@@ -5,14 +5,14 @@ Created on Sat Dec 25 11:55:06 2021
 @author: Fiona
 """
 import game
-from pytest import fixture
-import logging
 
-CARDS_IN_HAND=2
-NUMBER_OF_PLAYERS=1
-NUMBER_OF_PILES=2
-CARDS_PER_TURN=1
-NUMBER_OF_CARDS=10
+
+CARDS_IN_HAND = 2
+NUMBER_OF_PLAYERS = 1
+NUMBER_OF_PILES = 2
+CARDS_PER_TURN = 1
+NUMBER_OF_CARDS = 10
+
 
 def test_player_creation():
     """
@@ -23,11 +23,17 @@ def test_player_creation():
     None.
 
     """
-    my_game=game.Game(cards_in_hand=CARDS_IN_HAND, number_of_players=NUMBER_OF_PLAYERS, number_of_piles=NUMBER_OF_PILES, cards_per_turn=CARDS_PER_TURN, number_of_cards=NUMBER_OF_CARDS)
-    my_player=my_game.players[0]
-    assert my_player.is_my_turn==False
-    assert len(my_player.hand)==CARDS_IN_HAND
-    assert my_player.number_of_cards_i_need_to_play==CARDS_PER_TURN
+    my_game = game.Game(
+        cards_in_hand=CARDS_IN_HAND,
+        number_of_players=NUMBER_OF_PLAYERS,
+        number_of_piles=NUMBER_OF_PILES,
+        cards_per_turn=CARDS_PER_TURN,
+        number_of_cards=NUMBER_OF_CARDS)
+    my_player = my_game.players[0]
+    assert not my_player.is_my_turn
+    assert len(my_player.hand) == CARDS_IN_HAND
+    assert my_player.number_of_cards_i_need_to_play == CARDS_PER_TURN
+
 
 def test_pile_creation():
     """
@@ -38,19 +44,26 @@ def test_pile_creation():
     None.
 
     """
-    my_game=game.Game(cards_in_hand=CARDS_IN_HAND, number_of_players=NUMBER_OF_PLAYERS, number_of_piles=NUMBER_OF_PILES, cards_per_turn=CARDS_PER_TURN, number_of_cards=NUMBER_OF_CARDS)
-    my_decreasing_pile=my_game.piles[0]
-    my_increasing_pile=my_game.piles[1]
-    my_drawing_pile=my_game.drawing_pile
-    assert my_increasing_pile.cards==[game.LOWEST_PLAYABLE_NUMBER-1]
-    assert my_increasing_pile.id_number==1
-    assert my_increasing_pile.get_top_card()==game.LOWEST_PLAYABLE_NUMBER-1
+    my_game = game.Game(
+        cards_in_hand=CARDS_IN_HAND,
+        number_of_players=NUMBER_OF_PLAYERS,
+        number_of_piles=NUMBER_OF_PILES,
+        cards_per_turn=CARDS_PER_TURN,
+        number_of_cards=NUMBER_OF_CARDS)
+    my_decreasing_pile = my_game.piles[0]
+    my_increasing_pile = my_game.piles[1]
+    my_drawing_pile = my_game.drawing_pile
+    assert my_increasing_pile.cards == [game.LOWEST_PLAYABLE_NUMBER - 1]
+    assert my_increasing_pile.id_number == 1
+    assert my_increasing_pile.get_top_card() == game.LOWEST_PLAYABLE_NUMBER - 1
 
-    assert my_decreasing_pile.cards==[NUMBER_OF_CARDS]
-    assert my_decreasing_pile.id_number==0
-    assert my_decreasing_pile.get_top_card()==NUMBER_OF_CARDS
+    assert my_decreasing_pile.cards == [NUMBER_OF_CARDS]
+    assert my_decreasing_pile.id_number == 0
+    assert my_decreasing_pile.get_top_card() == NUMBER_OF_CARDS
 
-    assert len(my_drawing_pile.cards)==NUMBER_OF_CARDS-2-CARDS_IN_HAND*NUMBER_OF_PLAYERS
+    assert len(my_drawing_pile.cards) == NUMBER_OF_CARDS - \
+        2 - CARDS_IN_HAND * NUMBER_OF_PLAYERS
+
 
 def test_game_creation():
     """
@@ -61,7 +74,11 @@ def test_game_creation():
     None.
 
     """
-    my_game=game.Game(cards_in_hand=CARDS_IN_HAND, number_of_players=NUMBER_OF_PLAYERS, number_of_piles=NUMBER_OF_PILES, cards_per_turn=CARDS_PER_TURN, number_of_cards=NUMBER_OF_CARDS)
-    assert len(my_game.piles)==NUMBER_OF_PILES
-    assert len(my_game.players)==NUMBER_OF_PLAYERS
-
+    my_game = game.Game(
+        cards_in_hand=CARDS_IN_HAND,
+        number_of_players=NUMBER_OF_PLAYERS,
+        number_of_piles=NUMBER_OF_PILES,
+        cards_per_turn=CARDS_PER_TURN,
+        number_of_cards=NUMBER_OF_CARDS)
+    assert len(my_game.piles) == NUMBER_OF_PILES
+    assert len(my_game.players) == NUMBER_OF_PLAYERS
