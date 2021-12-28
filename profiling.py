c18501477b333e0ddf65ce7add682d1aa5732daf
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-12-28 16:52:28 +0000

    putting text files into separate folder

--- profiling.py
+++ profiling.py
@@ -11,15 +11,16 @@ import re
 import plotting
 import pstats
 from pstats import SortKey
-import game_logging
-import game_test
 
 
 
 number_of_cards_array=[*range(10, 110, 10)]
 cProfile.run('plotting.TestStrategy.run_tests(number_of_cards_array)', 'profiling_log')
-#cProfile.run('logging_test()', 'profiling')
 
 p = pstats.Stats('profiling_log')
+# sort by cumulative time
 p.strip_dirs().sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats()
+
+# # sort by number of calls performed by each function
+# p.strip_dirs().sort_stats(SortKey.CALLS).print_stats()
 
