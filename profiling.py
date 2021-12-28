4376909e546698bb83b2a9ab362a7bf2bd6c7f5d
Fiona Lapp <fiona.lapp.fg@gmail.com>
2021-12-28 10:30:32 +0000

    Turns out it was a 'Did you try closing and opening it?' kinda problem. No idea what caused it.

--- profiling.py
+++ profiling.py
@@ -5,14 +5,22 @@ Created on Tue Dec 28 07:59:04 2021
 @author: Fiona
 """
 
+import profile
 import cProfile
 import re
 import plotting
 import pstats
 from pstats import SortKey
+import game_logging
 
-number_of_cards_array=[*range(10, 50, 10)]
-cProfile.run('plotting.TestStrategy.run_tests(number_of_cards_array)')
-# cProfile.run('plotting.TestStrategy.run_tests(number_of_cards_array)', 'profiling')
-# p = pstats.Stats('profiling')
-# p.strip_dirs().sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats()
+# def logging_test():
+#     with game_logging.ContextManager():
+#         print(number_of_cards_array)
+
+number_of_cards_array=[*range(10, 110, 10)]
+cProfile.run('plotting.TestStrategy.run_tests(number_of_cards_array)', 'profiling')
+#cProfile.run('logging_test()', 'profiling')
+
+p = pstats.Stats('profiling')
+p.strip_dirs().sort_stats(SortKey.CUMULATIVE, SortKey.TIME).print_stats()
+
