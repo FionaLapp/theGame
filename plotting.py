69f101fb410c856c2b154be843628bf3d94a47b3
Fiona Lapp <fiona.lapp.fg@gmail.com>
2022-01-12 13:32:08 +0000

    Slightly changed structure of code to make it more reusable

--- plotting.py
+++ plotting.py
@@ -13,11 +13,11 @@ import os
 # importing cProfile
 import cProfile #to check what takes longest
 
+#%% make plots pretty
 NUMBER_OF_ATTEMPTS = 100
 MARKER = 'o'
 COLOR= 'w'
-COLOR_1 = 'g'
-COLOR_2 = 'b'
+PLOTTING_COLORS=['g', 'b', 'r', 'm', 'c']
 BACKGROUND_COLOR = 'k'
 EDGE_COLOR='w'
 FONT_COLOR='w'
@@ -28,6 +28,7 @@ TITLE_SIZE = 25
 TITLE_DICT={'fontsize': TITLE_SIZE, 'color': FONT_COLOR}
 
 
+#%% running tests
 class TestStrategy():
     def run_one_test(number_of_attempts=100, cards_in_hand=6,
                  number_of_players=4, number_of_piles=4, cards_per_turn=2,
@@ -49,66 +50,80 @@ class TestStrategy():
                 "piles: {}".format(my_strategy.game.print_piles()))
             if my_strategy.game.game_won():
                 win_array[i] = 1
-        print(np.sum(win_array)/number_of_attempts)
+        #print(np.sum(win_array)/number_of_attempts)
         return np.sum(win_array)/number_of_attempts
 
-    def run_tests(number_of_cards_array):
+    def run_tests(strategies, number_of_cards_array=[*range(10, 110, 10)], number_of_players_array=[*range(1, 10, 1)], number_of_piles_array=[*range(2, 8, 2)]):
         with game_logging.ContextManager() as manager:
 
-            number_of_players_array=[*range(1, 10, 1)]
-            number_of_piles_array=[*range(2, 8, 2)]
-            number_of_cards_winning_percentage_first_strategy = np.zeros(len(number_of_cards_array))
-            number_of_cards_winning_percentage_second_strategy = np.zeros(len(number_of_cards_array))
+            winning_percentage = np.zeros((len(strategies), len(number_of_cards_array)))
 
-            number_of_players_winning_percentage = np.zeros(len(number_of_players_array))
-            number_of_piles_winning_percentage = np.zeros(len(number_of_piles_array))
             for i, j in enumerate(number_of_cards_array):
-                number_of_cards_winning_percentage_first_strategy[i] = TestStrategy.run_one_test(
-                    number_of_attempts=NUMBER_OF_ATTEMPTS, number_of_cards=j, strategy=the_game.PlayWithMetricStrategy)
-                number_of_cards_winning_percentage_second_strategy[i] = TestStrategy.run_one_test(
-                    number_of_attempts=NUMBER_OF_ATTEMPTS, number_of_cards=j, strategy=the_game.PlayWithDistanceCutoffStrategy)
+                for s, strategy in enumerate(strategies):
+                    winning_percentage[ s, i] = TestStrategy.run_one_test(
+                        number_of_attempts=NUMBER_OF_ATTEMPTS, number_of_cards=j, strategy=strategy)
 
-            # for i, j in enumerate(number_of_players_array):
-            #     number_of_players_winning_percentage[i] = TestStrategy.run_test(
-            #         number_of_attempts=NUMBER_OF_ATTEMPTS, number_of_players=j, number_of_cards=50)
-            # for i, j in enumerate(number_of_piles_array):
-            #     number_of_cards_winning_percentage[i] = TestStrategy.run_test(
-            #         number_of_attempts=NUMBER_OF_ATTEMPTS, number_of_piles=j, number_of_cards=100)
-            return number_of_cards_winning_percentage_first_strategy, number_of_cards_winning_percentage_second_strategy
 
+            return winning_percentage
 #%% plotting helper
 
-def draw_plot(x_data, y_data, label, x_label, y_label, position):
+def draw_plot(x_data, y_data, labels, x_label, y_label, title, position=111):
+    """
+
+
+    Parameters
+    ----------
+    x_data : numpy array (one-dimensional)
+        the x-values of the data points
+    y_data : numpy array (can be two-dimensional if plotting more than line in one figure).
+        Each row represents one line, the row-length must match that of the x-data
+    labels : list
+        list of labels for the y-data. The length must match the number of rows in the y-data array
+    x_label : String
+        x axis label
+    y_label : String
+        y-axis-label
+    title : String
+        Title of the graph
+    position : int
+        position of subplot (use 111 if only plotting one subplot)
+
+    Returns
+    -------
+    None.
+
+    """
     ax = fig.add_subplot(position, facecolor=BACKGROUND_COLOR)
     ax.tick_params(color=FONT_COLOR, labelcolor=FONT_COLOR)
-    ax.plot(x_data, y_data[:,0], marker=MARKER, color=COLOR_1, label=label, linewidth=LINEWIDTH)
-    ax.plot(x_data, y_data[:,1], marker=MARKER, color=COLOR_2, label=label, linewidth=LINEWIDTH)
+    for i in range(len(labels)):
+        ax.plot(x_data, y_data[:,i], marker=MARKER, color=PLOTTING_COLORS[i], label=labels[i], linewidth=LINEWIDTH)
 
-    ax.legend(labelcolor=[COLOR_1, COLOR_2])
+    ax.legend(labelcolor=PLOTTING_COLORS[:len(labels)])
     for spine in ax.spines.values():
         spine.set_edgecolor(EDGE_COLOR)
     ax.set_ylabel(y_label, color=COLOR)
-    ax.set_ylabel(x_label, color=COLOR)
+    ax.set_xlabel(x_label, color=COLOR)
     ax.set_title(label, color=COLOR)
-    plt.title(label)
+    plt.title(title)
 
 #%% main
 
 if __name__ == "__main__":
+    strategies=[the_game.PlayWithMetricStrategy, the_game.PlayWithDistanceCutoffStrategy]
     number_of_cards_array=[*range(10, 110, 10)]
-    number_of_cards_winning_percentage_first_strategy, number_of_cards_winning_percentage_second_strategy = TestStrategy.run_tests(number_of_cards_array)
+    winning_percentage = TestStrategy.run_tests(strategies, number_of_cards_array=[*range(10, 110, 10)])
 
 
 
     #%% plotting
     label =['number_of_cards', 'number_of_players', 'number_of_piles']
-    x_label = ['number_of_cards', 'number_of_players', 'number_of_piles']
-    y_label = ['success percentage','success percentage','success percentage']
-    #x_data= [number_of_cards_winning_percentage_first_strategy, number_of_players_winning_percentage, number_of_piles_winning_percentage]
-    position=[111, 111,111]# [311, 312, 313]
+    x_label = ['number_of_cards (in the game)' , 'number_of_players', 'number_of_piles']
+    y_label = 3*['success percentage']
+
+    title="comparison between always playing 2 cards and potentially playing more cards"
     fig = plt.figure(figsize=FIG_SIZE, facecolor=BACKGROUND_COLOR, edgecolor=EDGE_COLOR)
     i=0
-    draw_plot(number_of_cards_array, np.transpose(np.array([number_of_cards_winning_percentage_first_strategy, number_of_cards_winning_percentage_second_strategy])), ["PlayWithMetric", "PlayWithDistanceCutoff"], x_label[i], y_label[i], position[i])
+    draw_plot(number_of_cards_array, np.transpose(winning_percentage), ["PlayWithMetric", "PlayWithDistanceCutoff"], x_label[i], y_label[i], title)
 
     plt.show()
 
