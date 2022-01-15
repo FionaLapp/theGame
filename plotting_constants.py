219b94478577555d08c7a2524957f533df42c5e0
Fiona Lapp <fiona.lapp.fg@gmail.com>
2022-01-15 19:13:28 +0000

    Putting colours, etc. in separate file

---
+++ plotting_constants.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 15 18:03:11 2022
+
+@author: Fiona
+"""
+
+#%% make plots pretty
+NUMBER_OF_GAMES = 100
+MARKER = 'o'
+COLOR= 'w'
+PLOTTING_COLORS=['g', 'b', 'r', 'm', 'c']
+BACKGROUND_COLOR = 'k'
+EDGE_COLOR='w'
+FONT_COLOR='w'
+FIG_SIZE = [6, 4]
+FONTSIZE = 18
+LINEWIDTH = 3
+TITLE_SIZE = 25
+TITLE_DICT={'fontsize': TITLE_SIZE, 'color': FONT_COLOR}
