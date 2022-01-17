340a177b1d323e5fca752c2745882db41aee573f
Fiona Lapp <fiona.lapp.fg@gmail.com>
2022-01-17 18:45:06 +0000

    Running the deep q rl model

---
+++ test_deep_q_rl.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 17:10:06 2022
+
+@author: Fiona
+"""
+
+
+from tensorflow.keras import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.optimizers import Adam
+from collections import deque
+import numpy as np
+import tensorflow as tf
+import random
+from tqdm import tqdm
+import time
+import pdb
+
+import the_game
+import matplotlib.pyplot as plt
+import os
+import plotting_constants as c
+import pandas as pd
+from attempt_3_deep_q_rl import Environment
+import attempt_3_deep_q_rl
+
+CARDS_IN_HAND = 6
+NUMBER_OF_PLAYERS = 2
+NUMBER_OF_PILES = 4
+CARDS_PER_TURN = 1
+NUMBER_OF_CARDS = 100
+
+#%%
+
+class DRLStrategy(the_game.Strategy):
+    def __init__(self, cards_in_hand=6, number_of_players=4, number_of_piles=4,
+                   cards_per_turn=1, number_of_cards=NUMBER_OF_CARDS):
+          self.env=Environment()
+          self.model=tf.keras.models.load_model("models/model___100.00max__100.00avg__100.00min__1642437186.model")
+
+    def predict_action(self, state):
+        return np.argmax(self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0])
+
+
+    def play(self):
+        self.actions=np.zeros(3)
+        while True:
+              if self.env.game.game_finished():
+                  return self.actions
+              else:
+
+                  state=attempt_3_deep_q_rl.get_state(self.env.game)
+                  action=self.predict_action(state)
+                  self.actions[action]+=1
+                  self.env.step(action)
+
+
+
+    def start_game(self):
+        self.env.game.current_player = self.env.game.players[0]
+        self.env.game.calculate_basic_metric()
+
+def run_tests(number_of_tests):
+    strategy=DRLStrategy()
+    win_array=np.zeros(number_of_tests)
+    jump_array=np.zeros(number_of_tests)
+    actions_array=np.zeros((number_of_tests, 3))
+
+    for i in range(number_of_tests):
+
+        strategy.env.reset()
+        strategy.start_game()
+        actions=strategy.play()
+        print(actions)
+        actions_array[i,:]=actions
+        jump_array[i]=strategy.env.game.jump_counter
+        if strategy.env.game.game_won():
+            win_array[i]=1
+        print("{}% done".format((i+1)/number_of_tests*100))
+    return win_array, jump_array, actions_array
+
+win_array, jump_array, actions_array= run_tests(100)
+
